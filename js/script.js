c0d6fae787be3e1c80ceb395814fe98fa6b91492
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2020-12-21 23:06:41 +0100

    User interaction

--- js/script.js
+++ js/script.js
@@ -19,3 +19,11 @@ console.log(obj["age"]);
 //Array
 let arr = ["plum.png", "orange.jpg", "apple.bnp"];
 console.log(arr[1]);
+
+
+
+const answers = [];
+
+answers[0] = prompt("What's your name", "");
+answers[1] = prompt("What's your surname", "");
+answers[2] = prompt("What's your age", "");
