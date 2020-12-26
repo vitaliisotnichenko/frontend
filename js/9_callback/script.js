2b3dcddb3fa4297823752595422a62deae44c279
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2020-12-26 19:11:48 +0100

    callback functions

---
+++ js/9_callback/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function first() {
+    // Do something
+    setTimeout(function () {
+        console.log('1');
+    }, 500);
+}
+
+function second() {
+    console.log('2');
+}
+
+first();
+second();
+
+function learnJS (lang, callback) {
+     console.log(`I learn: ${lang}`);
+     callback();
+}
+
+function done () {
+    console.log('I have finished this lesson');
+}
+learnJS('Javascript', done);
